a08139dd5df183f583d3cff509bf6821c10cf213
Gabriel Valle <gvalle91@gmail.com>
2018-06-29 03:15:50 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,7 +60,7 @@ class InteractiveRecord
       SELECT * FROM #{self.table_name}
       WHERE ? = ?
     SQL
-    DB[:conn].execute(sql, hash.keys.first, hash.values.first)
+    DB[:conn].execute(sql, send(hash.keys.first), hash.values.first)
   end
 
 end
